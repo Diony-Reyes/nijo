a979566a710f8992c3f9dd51d30c12181439dfde
Diony-Reyes <40726544+Diony-Reyes@users.noreply.github.com>
2020-04-22 23:26:18 -0400

    Add files via upload

---
+++ assets/js/swiper-ac.js
@@ -0,0 +1,29 @@
+var swContainerHeight;
+
+var mainSwiper = new Swiper(".swiper-container", {
+    spaceBetween: 0,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+    },
+    effect: "fade",
+    speed: 800,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    }
+});
+
+var swContainer = document.querySelector("#main .swiper-container");
+swContainer.style.height = innerHeight;
+
+window.addEventListener("resize", ev => {
+    swContainerHeight = swContainer.clientHeight;
+    swContainer.style.height = innerHeight;
+
+    if (swContainerHeight !== innerHeight) {
+        window.location = "/";
+    }
+});
